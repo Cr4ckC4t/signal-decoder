e43061086b4131db9a455d77604be5e690c90a4c
crackcat <63863112+Cr4ckC4t@users.noreply.github.com>
2024-10-17 22:46:38 +0200

    Create DissectorBase.py

---
+++ DissectorBase.py
@@ -0,0 +1,26 @@
+class DissectorBase:
+	def __init__(self):
+		self._input_vars = {}
+
+	def ingest(self, raw: str) -> None:
+		# append possible/valid input variations to self.input_vars
+		pass
+
+	def add_variant(self, variant: str, name: str) -> None:
+		self._input_vars[name] = variant
+
+	def filter(self, needle: str, ignore_case: bool) -> None:
+		"""
+		Remove all variants that dont contain the needle
+		"""
+		self._input_vars = { name: variant
+		  for name, variant
+		  in self._input_vars.items()
+		  if (needle in variant) or
+		    (ignore_case and
+		    ((needle.lower() in variant) or (needle.upper() in variant)))
+		}
+
+	def show_variants(self):
+		for name,variant in self._input_vars.items():
+			print(f'======\n{name}: {variant}')
